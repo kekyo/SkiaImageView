ebca228d71fce7a48c4f50807afcd2009d4412a9
Kouji Matsui <k@kekyo.net>
2022-11-13 10:48:55 +0900

    Refactored.

--- SkiaImageView.Avalonia/SKImageView.cs
+++ SkiaImageView.Avalonia/SKImageView.cs
@@ -13,77 +13,76 @@ using Avalonia.Controls;
 using Avalonia.Media;
 using Avalonia.Metadata;
 
-namespace SkiaImageView
+namespace SkiaImageView;
+
+partial class SKImageView : Control
 {
-    partial class SKImageView : Control
+    public static readonly AvaloniaProperty<object?> SourceProperty =
+        Interops.Register<object?, SKImageView>(
+            nameof(Source), null, d => d.OnBitmapChanged());
+
+    public static readonly AvaloniaProperty<Stretch> StretchProperty =
+        Interops.Register<Stretch, SKImageView>(
+            nameof(Stretch), Stretch.None, d => d.Invalidate(false));
+
+    public static readonly AvaloniaProperty<StretchDirection> StretchDirectionProperty =
+        Interops.Register<StretchDirection, SKImageView>(
+            nameof(StretchDirection), StretchDirection.Both, d => d.Invalidate(false));
+
+    public static readonly AvaloniaProperty<RenderMode> RenderModeProperty =
+        Interops.Register<RenderMode, SKImageView>(
+            nameof(RenderMode), RenderMode.AsynchronouslyForFetching, d => d.OnBitmapChanged());
+
+    public static readonly AvaloniaProperty<ProjectionQuality> ProjectionQualityProperty =
+        Interops.Register<ProjectionQuality, SKImageView>(
+            nameof(ProjectionQuality),
+            ProjectionQuality.Perfect,
+            _ => { });
+
+    static SKImageView()
     {
-        public static readonly AvaloniaProperty<object?> SourceProperty =
-            Interops.Register<object?, SKImageView>(
-                nameof(Source), null, d => d.OnBitmapChanged());
+        AffectsRender<SKImageView>(SourceProperty, StretchProperty, StretchDirectionProperty);
+        AffectsMeasure<SKImageView>(SourceProperty, StretchProperty, StretchDirectionProperty);
+    }
 
-        public static readonly AvaloniaProperty<Stretch> StretchProperty =
-            Interops.Register<Stretch, SKImageView>(
-                nameof(Stretch), Stretch.None, d => d.Invalidate(false));
-
-        public static readonly AvaloniaProperty<StretchDirection> StretchDirectionProperty =
-            Interops.Register<StretchDirection, SKImageView>(
-                nameof(StretchDirection), StretchDirection.Both, d => d.Invalidate(false));
-
-        public static readonly AvaloniaProperty<RenderMode> RenderModeProperty =
-            Interops.Register<RenderMode, SKImageView>(
-                nameof(RenderMode), RenderMode.AsynchronouslyForFetching, d => d.OnBitmapChanged());
-
-        public static readonly AvaloniaProperty<ProjectionQuality> ProjectionQualityProperty =
-            Interops.Register<ProjectionQuality, SKImageView>(
-                nameof(ProjectionQuality),
-                ProjectionQuality.Perfect,
-                _ => { });
-
-        static SKImageView()
+    private void Invalidate(bool both)
+    {
+        if (both)
         {
-            AffectsRender<SKImageView>(SourceProperty, StretchProperty, StretchDirectionProperty);
-            AffectsMeasure<SKImageView>(SourceProperty, StretchProperty, StretchDirectionProperty);
+            base.InvalidateMeasure();
         }
+        base.InvalidateVisual();
+    }
 
-        private void Invalidate(bool both)
+    [Content]
+    public object? Source
+    {
+        get => this.GetValue(SourceProperty);
+        set => this.SetValue(SourceProperty, value);
+    }
+
+    private Size RenderSize =>
+        this.Bounds.Size;
+
+    private void UpdateWith(BackingStore? backingStore)
+    {
+        this.backingStore = backingStore;
+        this.Invalidate(true);
+    }
+
+    protected override Size MeasureOverride(Size constraint) =>
+        this.InternalMeasureArrangeOverride(constraint);
+
+    protected override Size ArrangeOverride(Size arrangeSize) =>
+        this.InternalMeasureArrangeOverride(arrangeSize);
+
+    public override void Render(DrawingContext drawingContext)
+    {
+        base.Render(drawingContext);
+
+        if (this.backingStore is { } backingStore)
         {
-            if (both)
-            {
-                base.InvalidateMeasure();
-            }
-            base.InvalidateVisual();
-        }
-
-        [Content]
-        public object? Source
-        {
-            get => this.GetValue(SourceProperty);
-            set => this.SetValue(SourceProperty, value);
-        }
-
-        private Size RenderSize =>
-            this.Bounds.Size;
-
-        private void UpdateWith(BackingStore? backingStore)
-        {
-            this.backingStore = backingStore;
-            this.Invalidate(true);
-        }
-
-        protected override Size MeasureOverride(Size constraint) =>
-            this.InternalMeasureArrangeOverride(constraint);
-
-        protected override Size ArrangeOverride(Size arrangeSize) =>
-            this.InternalMeasureArrangeOverride(arrangeSize);
-
-        public override void Render(DrawingContext drawingContext)
-        {
-            base.Render(drawingContext);
-
-            if (this.backingStore is { } backingStore)
-            {
-                backingStore.Draw(drawingContext, this.RenderSize);
-            }
+            backingStore.Draw(drawingContext, this.RenderSize);
         }
     }
 }
--- SkiaImageView.Wpf/SKImageView.cs
+++ SkiaImageView.Wpf/SKImageView.cs
@@ -12,67 +12,66 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
-namespace SkiaImageView
+namespace SkiaImageView;
+
+partial class SKImageView : FrameworkElement
 {
-    partial class SKImageView : FrameworkElement
+    public static readonly DependencyProperty SourceProperty =
+        Interops.Register<object?, SKImageView>(
+            nameof(Source), null, d => d.OnBitmapChanged());
+
+    public static readonly DependencyProperty StretchProperty =
+        Interops.Register<Stretch, SKImageView>(
+            nameof(Stretch), Stretch.None, d => d.Invalidate(false));
+
+    public static readonly DependencyProperty StretchDirectionProperty =
+        Interops.Register<StretchDirection, SKImageView>(
+            nameof(StretchDirection), StretchDirection.Both, d => d.Invalidate(false));
+
+    public static readonly DependencyProperty RenderModeProperty =
+        Interops.Register<RenderMode, SKImageView>(
+            nameof(RenderMode), RenderMode.AsynchronouslyForFetching, d => d.OnBitmapChanged());
+
+    public static readonly DependencyProperty ProjectionQualityProperty =
+        Interops.Register<ProjectionQuality, SKImageView>(
+            nameof(ProjectionQuality),
+            ProjectionQuality.Perfect,
+            _ => { });
+
+    private void Invalidate(bool both)
     {
-        public static readonly DependencyProperty SourceProperty =
-            Interops.Register<object?, SKImageView>(
-                nameof(Source), null, d => d.OnBitmapChanged());
-
-        public static readonly DependencyProperty StretchProperty =
-            Interops.Register<Stretch, SKImageView>(
-                nameof(Stretch), Stretch.None, d => d.Invalidate(false));
-
-        public static readonly DependencyProperty StretchDirectionProperty =
-            Interops.Register<StretchDirection, SKImageView>(
-                nameof(StretchDirection), StretchDirection.Both, d => d.Invalidate(false));
-
-        public static readonly DependencyProperty RenderModeProperty =
-            Interops.Register<RenderMode, SKImageView>(
-                nameof(RenderMode), RenderMode.AsynchronouslyForFetching, d => d.OnBitmapChanged());
-
-        public static readonly DependencyProperty ProjectionQualityProperty =
-            Interops.Register<ProjectionQuality, SKImageView>(
-                nameof(ProjectionQuality),
-                ProjectionQuality.Perfect,
-                _ => { });
-
-        private void Invalidate(bool both)
+        if (both)
         {
-            if (both)
-            {
-                base.InvalidateMeasure();
-            }
-            base.InvalidateVisual();
+            base.InvalidateMeasure();
         }
+        base.InvalidateVisual();
+    }
 
-        public object Source
+    public object Source
+    {
+        get => this.GetValue(SourceProperty);
+        set => this.SetValue(SourceProperty, value);
+    }
+
+    private void UpdateWith(BackingStore? backingStore)
+    {
+        this.backingStore = backingStore;
+        this.Invalidate(true);
+    }
+
+    protected override Size MeasureOverride(Size constraint) =>
+        this.InternalMeasureArrangeOverride(constraint);
+
+    protected override Size ArrangeOverride(Size arrangeSize) =>
+        this.InternalMeasureArrangeOverride(arrangeSize);
+
+    protected override void OnRender(DrawingContext drawingContext)
+    {
+        base.OnRender(drawingContext);
+    
+        if (this.backingStore is { } backingStore)
         {
-            get => this.GetValue(SourceProperty);
-            set => this.SetValue(SourceProperty, value);
-        }
-
-        private void UpdateWith(BackingStore? backingStore)
-        {
-            this.backingStore = backingStore;
-            this.Invalidate(true);
-        }
-
-        protected override Size MeasureOverride(Size constraint) =>
-            this.InternalMeasureArrangeOverride(constraint);
-
-        protected override Size ArrangeOverride(Size arrangeSize) =>
-            this.InternalMeasureArrangeOverride(arrangeSize);
-
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            base.OnRender(drawingContext);
-        
-            if (this.backingStore is { } backingStore)
-            {
-                backingStore.Draw(drawingContext, this.RenderSize);
-            }
+            backingStore.Draw(drawingContext, this.RenderSize);
         }
     }
 }
